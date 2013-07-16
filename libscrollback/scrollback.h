b7348f5014deb1b80f998a1963ea044bf973b280
Alex Jordan <alexander3223098@gmail.com>
2013-07-16 16:27:58 -0700

    initial method definitions

---
+++ libscrollback/scrollback.h
@@ -0,0 +1,42 @@
+/*
+ * libscrollback
+ * written by Alex Jordan
+ */
+
+bool Libscrollback_initialized = false;
+
+int Libscrollback_initialize()
+{
+  //any init here
+  Libscrollback_initialized = true;
+}
+
+void Libscrollback_setMaxBufferSize()
+{
+
+}
+
+void Libscrollback_addToBuffer()
+{
+
+}
+
+void Libscrollback_reserveLine()
+{
+
+}
+
+void Libscrollback_unreserveLine()
+{
+
+}
+
+bool Libscrollback_getReservedLine()
+{
+
+}
+
+void Libscrollback_redraw()
+{
+
+}
