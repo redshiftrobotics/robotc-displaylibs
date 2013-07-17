9cb30d66142907d91029079bb3064260d1e02828
Alex Jordan <alexander3223098@gmail.com>
2013-07-17 11:41:29 -0700

    add reserved lines support
    compilation functionality not guaranteed.

--- libscrollback/scrollback.h
+++ libscrollback/scrollback.h
@@ -4,11 +4,14 @@
  */
 
 bool Libscrollback_initialized = false;
+int Libscrollback_maxBufferSize = 100;
+bool Libscrollback_lineReserved[9] = false;
+
 
 int Libscrollback_initialize()
 {
-  //any init here
-  Libscrollback_initialized = true;
+	 //any init here
+	Libscrollback_initialized = true;
 }
 
 void Libscrollback_setMaxBufferSize()
@@ -21,19 +24,21 @@ void Libscrollback_addToBuffer()
 
 }
 
-void Libscrollback_reserveLine()
+int Libscrollback_reserveLine(int line)
 {
-
+	if ( line >= 0 && line <= 9 ) Libscrollback_lineReserved[line] = true; else return 1;
+	return 0;
 }
 
 void Libscrollback_unreserveLine()
 {
-
+	if ( line >= 0 && line <= 9 ) Libscrollback_lineReserved[line] = false; else return 1;
+ 	return 0;
 }
 
 bool Libscrollback_getReservedLine()
 {
-
+	if ( line >= 0 && line <= 9 ) return Libscrollback_lineReserved[line]; else return -1;
 }
 
 void Libscrollback_redraw()
