3d036a58b13d53e1386585df9f3e688388f1da49
Alex Jordan <alexander3223098@gmail.com>
2013-08-03 18:25:23 -0700

    add WIP draw method

--- libscrollback/scrollback.h
+++ libscrollback/scrollback.h
@@ -1,26 +1,37 @@
 /*
- * libscrollback
- * written by Alex Jordan
+ *  libscrollback
+ *  written by Alex Jordan
  */
 
+const int Libscrollback_screenSize = 9;
 bool Libscrollback_initialized = false;
-int Libscrollback_maxBufferSize = 100;
-bool Libscrollback_lineReserved[9] = false;
+const int Libscrollback_maxBufferSize = 100;
+String Libscrollback_buffer[Libscrollback_maxBufferSize] = ""; // set to an empty string just in case we get problems later for accessing null strings
+bool Libscrollback_lineReserved[Libscrollback_screenSize] = false;
 
 int Libscrollback_initialize()
 {
-	 //any init here
+	// probably deprecated because robotc doesn't have malloc()
+	//any init here
 	Libscrollback_initialized = true;
 }
 
 void Libscrollback_setMaxBufferSize()
 {
-
+	// probably deprecated because robotc doesn't have malloc()
 }
 
-void Libscrollback_addToBuffer()
+void Libscrollback_addToBuffer(String stringToAdd)
 {
-
+	// push the stack up
+	// note: implemented as a "pull", not a "push"
+	// the top of the stack gets overwritten as a part of this
+	for (int i = 99; i > 1; i--)
+	{
+		Libscrollback_buffer[i] = Libscrollback_buffer[i-1];
+	}
+	// handle the very bottom specially, since that's where we insert the next buffer item
+	Libscrollback_buffer[0] = stringToAdd;
 }
 
 int Libscrollback_reserveLine(int line)
@@ -42,5 +53,12 @@ bool Libscrollback_getReservedLine()
 
 void Libscrollback_redraw()
 {
-
+	// figure out how many lines we have available
+	int avail = 0;
+	for (int i = 0; i < Libscrollback_screenSize; i++)
+	{
+		if Libscrollback_getReservedLine(i) /* do nothing */ else avail++;
+	}
+	// construct a temporary array with the amount of lines we have
+	String line[avail];
 }
